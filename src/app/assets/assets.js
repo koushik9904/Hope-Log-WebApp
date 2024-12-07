d205b5c0a6f2d20ab52afce2070524a6ffefdc72
LogicUI <johnwee35@gmail.com>
2024-12-07 17:50:33 +0800

    Add AssetsJS for icons

---
+++ src/app/assets/assets.js
@@ -0,0 +1,11 @@
+import BookIcon from './icons/book-solid.svg';
+import FaceIcon from './icons/face-smile-beam-regular.svg'
+import GlobeIcon from './icons/globe-solid.svg';
+import HopeIcon from './icons/hope.svg'
+import Seedling from './icons/seedling-solid.svg';
+
+export const BookIconSVG = BookIcon
+export const FaceIconSVG = FaceIcon
+export const GlobeIconSVG = GlobeIcon
+export const HopeIconSVG = HopeIcon 
+export const SeedlingIconSVG = Seedling
