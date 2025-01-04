5b4ecfc98f82811c8292b056eb8bcd08e4b7d775
LogicUI <johnwee35@gmail.com>
2025-01-04 21:35:57 +0800

    create protected routes with auth ensuring user is logged in before using entry or feelings ai page

---
+++ src/app/HOC/WithAuth.tsx
@@ -0,0 +1,35 @@
+import React, { ComponentType, useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
+import { BounceLoader } from 'react-spinners';
+import { toast } from 'react-toastify';
+
+const withAuth = (WrappedComponent: ComponentType) => {
+    const ProtectedRoute: React.FC = (props) => {
+        const router = useRouter();
+        const [isLoading, setIsLoading] = useState(true);
+
+        useEffect(() => {
+            const authToken = localStorage.getItem('authToken');
+            if (!authToken) {
+                router.push('/');
+                toast.error('You need to login to access this page');
+            } else {
+                setIsLoading(false);
+            }
+        }, [router]);
+
+        if (isLoading) {
+            return (
+                <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
+                    <BounceLoader color="#4A90E2" />
+                </div>
+            )
+        }
+
+        return <WrappedComponent {...props} />;
+    };
+
+    return ProtectedRoute;
+};
+
+export default withAuth;
