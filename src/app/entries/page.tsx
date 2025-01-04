c6fbf0c6fefc81d9eff127da896f706582c0197e
LogicUI <johnwee35@gmail.com>
2025-01-04 21:35:38 +0800

    update entries to use user entires

--- src/app/entries/page.tsx
+++ src/app/entries/page.tsx
@@ -1,48 +1,30 @@
 'use client'
 import { useState } from "react";
 import { useRouter } from 'next/navigation'
-import moment from "moment";
-import { SearchIconSVG, WriteIconSVG, TrashSVG } from "../assets/assets";
-import Image from "next/image";
 import {Modal} from 'react-responsive-modal';
+import Image from "next/image";
+import { WriteIconSVG, TrashSVG } from "../assets/assets";
+import WithAuth from "../HOC/WithAuth";
+import {ConvoEntry} from "../types/types";
 import 'react-responsive-modal/styles.css';
+import useEntries from "../hooks/useEntries";
 
 
-type Entry = {
-    title: string;
-    date: string;
-    content: string;
-    analysis: string;
-};
 
-const mockEntries = [
-    {
-        title: "Feeling Excited About Building An App",
-        date: moment().subtract(1, "days").format("dddd, MMMM Do @ h:mm a"),
-        content:
-            "I feel so excited about starting this app for the hackathon. I've been thinking about all the features I could include, and it’s sparking so many creative ideas in my mind. This project feels like a fresh challenge, and it’s giving me a real sense of purpose and motivation. I can’t wait to dive deeper into it!",
-        analysis:
-            "Embracing this excitement can fuel your productivity and innovation as you work on your app. Channeling this energy into your project might lead to rewarding outcomes.",
-    },
-    {
-        title: "Feeling Lethargic From Lack Of Sleep",
-        date: moment().format("dddd, MMMM Do @ h:mm a"),
-        content:
-            "I’ve been feeling so drained today because I didn’t get enough sleep last night. My energy levels are really low, and it’s been hard to focus on anything. I know I need to take better care of myself and prioritize rest, but it’s been tough to stick to a consistent schedule lately.",
-        analysis:
-            "Acknowledging the importance of rest can help you regain balance and function effectively. Consider creating a better sleep routine.",
-    },
-];
+
 
 const Entries = () => {
-    const [entries, setEntries] = useState<Array<Entry>>(mockEntries);
-    const [selectedEntry, setSelectedEntry] = useState<Entry | null>(null);
+
+    const {isLoading, data: entries, handleDeleteEntry} = useEntries()
+    const [selectedEntry, setSelectedEntry] = useState<ConvoEntry | null>(null);
     const [popoverVisible, setPopoverVisible] = useState<number | null>(null);
     const [detailsOpen, setDetailsOpen] = useState(false);
     const onOpenDetailsModal = () => setDetailsOpen(true);
     const onCloseDetailsModal = () => setDetailsOpen(false);
 
+
     const router = useRouter();
+
 
     return (
         <>
@@ -57,10 +39,10 @@ const Entries = () => {
                     {selectedEntry && (
                         <div className="text-white">
                             <h2 className="text-2xl mb-4">{selectedEntry.title}</h2>
-                            <p className="text-sm">{selectedEntry.date}</p>
+                            <p className="text-sm">{selectedEntry.created_at}</p>
                             <div className="mt-4">
                                 <h3 className="font-medium">Summary</h3>
-                                <p>{selectedEntry.content}</p>
+                                <p>{selectedEntry.summary}</p>
                             </div>
                             <div className="mt-4">
                                 <h3 className="font-medium">Analysis</h3>
@@ -70,30 +52,9 @@ const Entries = () => {
                     )}
                 </div>
             </Modal>
-            <div className="container mx-auto p-4 text-white">
+            <div className="container mx-auto ml-auto p-4 text-white">
                 <div className="flex justify-between items-center">
-                    <div className="relative w-full md:w-2/6 mb-4">
-                        <input
-                            type="text"
-                            placeholder="  Search entries..."
-                            className="w-full p-3 pl-12 rounded bg-message"
-                            onChange={(e) => {
-                                const searchTerm = e.target.value.toLowerCase();
-                                setEntries(
-                                    mockEntries.filter((entry) =>
-                                        entry.title.toLowerCase().includes(searchTerm)
-                                    )
-                                );
-                            }}
-                        />
-                        <button className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                            <Image
-                                src={SearchIconSVG}
-                                alt="search"
-                            />
-                        </button>
-                    </div>
-                    <button onClick={() => router.push("/chatbot")} className="bg-ascent hidden md:flex p-4 rounded flex items-center mb-4">
+                    <button onClick={() => router.push("/chatbot")} className="bg-ascent hidden md:flex p-4 rounded flex items-center mb-4 ml-auto">
                         <Image
                             src={WriteIconSVG}
                             width={15}
@@ -106,9 +67,8 @@ const Entries = () => {
                 <div className="flex flex-col md:flex-row gap-5">
 
                     <div className="bg-message rounded shadow-md w-full md:w-1/3">
-                        <h1 className="text-lg font-bold p-4 border-b">This Week</h1>
                         <div className="divide-y">
-                            {entries.map((entry, index) => (
+                            {entries && entries.entries.map((entry : ConvoEntry, index : number) => (
                                 <div
                                     key={index}
                                     className="p-4 border-b hover:bg-mutedHover cursor-pointer relative group"
@@ -144,7 +104,7 @@ const Entries = () => {
                                                 <button
                                                     className="bg-red-500 text-white px-3 py-1 rounded-lg hover:bg-red-600"
                                                     onClick={() => {
-                                                        setEntries(entries.filter((_, i) => i !== index)); 
+                                                        handleDeleteEntry(entry.id);
                                                         setPopoverVisible(null); 
                                                     }}
                                                 >
@@ -167,10 +127,10 @@ const Entries = () => {
                         {selectedEntry ? (
                             <div>
                                 <h2 className="text-2xl font-bold">{selectedEntry.title}</h2>
-                                <p className="text-sm text-white">{selectedEntry.date}</p>
+                                <p className="text-sm text-white">{selectedEntry.created_at}</p>
                                 <div className="mt-4">
                                     <h3 className="font-medium">Summary</h3>
-                                    <p>{selectedEntry.content}</p>
+                                    <p>{selectedEntry.summary}</p>
                                 </div>
                                 <div className="mt-4">
                                     <h3 className="font-medium">Analysis</h3>
@@ -196,4 +156,4 @@ const Entries = () => {
     );
 };
 
-export default Entries
+export default WithAuth(Entries)
