de106336f675090fa5d3f74461e0b3d3d6d279db
LogicUI <johnwee35@gmail.com>
2025-01-26 22:03:26 +0800

    edit header

--- src/app/layout/Header/Header.tsx
+++ src/app/layout/Header/Header.tsx
@@ -3,30 +3,26 @@
 import { useState } from 'react'
 import { Dialog, DialogPanel } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
-import { HopeIconSVG } from "../../assets/assets"
 import { useAuth } from '@/app/hooks/useAuth'
-import Image from "next/image";
 import Link from 'next/link'
 import { BounceLoader } from 'react-spinners';
+import { Link as ScrollLink } from 'react-scroll';
 
 const navigation = [
-    { name: 'Entries', href: '/entries' },
-    { name: 'Feelings AI', href: '/chatbot' },
+    { name: 'Features', scrollTo: 'features-landing' },
+    { name: 'How it works', scrollTo: 'how-journaling-works' },
+    { name: 'Pricing', scrollTo: 'pricing-landing' },
 ]
 
 export default function Header() {
     const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
-    const { handleLogout, logOutIsLoading, userMetaData, isLoggedIn, handleLogin } = useAuth()
-
-    const handleTestUserLogin = async () => {
-        await handleLogin("fbqpluyagxtruvmavn@hthlm.com", "testuser")
-    }
+    const { handleLogout, logOutIsLoading, userMetaData, isLoggedIn } = useAuth()
 
     return (
         <>
             {logOutIsLoading && (
                 <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
-                    <div className="text-center text-white">
+                    <div className="text-center text-white flex flex-col items-center">
                         <BounceLoader color="#ffffff" />
                         <p className="mt-4">Logout successful. Returning you to the homepage...</p>
                     </div>
@@ -35,12 +31,8 @@ export default function Header() {
             <header className="text-white bg-dark">
                 <nav aria-label="Global" className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8">
                     <div className="flex lg:flex-1">
-                        <Link href="/" className="-m-1.5 p-1.5 flex justify-center items-center ">
-                            <span className="sr-only">Your Company</span>
-                            <Image
-                                src={HopeIconSVG}
-                                alt="icon" />
-                            <h1 className="ml-3 text-xl fontWeight-semibold">HopeLog</h1>
+                        <Link href="/" className="-m-1.5 p-1.5 flex justify-center items-center">
+                            <h1 className="text-xl fontWeight-semibold">HopeLog</h1>
                         </Link>
                     </div>
                     <div className="flex lg:hidden">
@@ -54,12 +46,19 @@ export default function Header() {
                     </div>
                     <div className="hidden lg:flex lg:gap-x-12">
                         {navigation.map((item) => (
-                            <a key={item.name} href={item.href} className="text-sm/6 font-semibold">
+                            <ScrollLink
+                                key={item.name}
+                                to={item.scrollTo}
+                                style={{ cursor: 'pointer' }}
+                                smooth={true}
+                                duration={500}
+                                className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold hover:bg-white-50"
+                            >
                                 {item.name}
-                            </a>
+                            </ScrollLink>
                         ))}
                     </div>
-                    <div className="hidden lg:flex lg:flex-1 lg:justify-end items-center">
+                    <div className="hidden lg:flex lg:flex-1 lg:justify-end">
                         {isLoggedIn && userMetaData ? (
                             <>
                                 <span className="mr-4 text-sm/6 font-semibold">Welcome {userMetaData.name}</span>
@@ -68,16 +67,9 @@ export default function Header() {
                                 </Link>
                             </>
                         ) : (
-                            <>
-                                {!isLoggedIn &&
-                                    <button onClick={handleTestUserLogin} className="text-sm/6 font-semibold bg-ascent px-3 py-2 rounded-md mr-4">
-                                        Sign In With Test User
-                                    </button>
-                                }
-                                <Link href="/login" className="text-sm/6 font-semibold">
-                                    Log in <span aria-hidden="true">&rarr;</span>
-                                </Link>
-                            </>
+                            <Link href="/login" className="text-sm/6 font-semibold bg-ascent px-4 py-2 rounded-md text-white">
+                                Get Started
+                            </Link>
                         )}
                     </div>
                 </nav>
@@ -85,12 +77,8 @@ export default function Header() {
                     <div className="fixed inset-0 z-10" />
                     <DialogPanel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-dark text-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-white-900/10">
                         <div className="flex items-center justify-between">
-                            <Link href="/" className="-m-1.5 p-1.5 flex justify-center items-center">
-                                <span className="sr-only">Your Company</span>
-                                <Image
-                                    src={HopeIconSVG}
-                                    alt="icon"
-                                />
+                            <Link href="/" className="-m-1.5 p-1.5">
+                                <h1 className="text-xl fontWeight-semibold">HopeLog</h1>
                             </Link>
                             <button
                                 type="button"
@@ -105,39 +93,35 @@ export default function Header() {
                             <div className="-my-6 divide-y divide-white-500/10">
                                 <div className="space-y-2 py-6">
                                     {navigation.map((item) => (
-                                        <a
+                                        <ScrollLink
                                             key={item.name}
-                                            href={item.href}
+                                            style={{ cursor: 'pointer' }}
+                                            to={item.scrollTo}
+                                            smooth={true}
+                                            duration={500}
                                             className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold hover:bg-white-50"
                                         >
                                             {item.name}
-                                        </a>
+                                        </ScrollLink>
                                     ))}
-                                </div>
-                                <div className="py-6 text-center">
-                                    {isLoggedIn && userMetaData && (
-                                        <span className="block mb-4 text-base/7 font-semibold">Welcome {userMetaData.name}</span>
-                                    )}
-                                    {isLoggedIn ? (
-                                        <Link
-                                            onClick={handleLogout}
-                                            href="/"
-                                            className="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold hover:bg-gray-50"
-                                        >
-                                            Log out
-                                        </Link>
-                                    ) : (
-                                        !isLoggedIn && <>
-                                            <button onClick={handleTestUserLogin} className="text-base/7 font-semibold bg-ascent px-3 py-2.5 rounded-md mb-4">
-                                                Sign In With Test User
-                                            </button>
+                                    {isLoggedIn && userMetaData ? (
+                                        <div className="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-white">
+                                            <span className="block mb-2">Welcome {userMetaData.name}</span>
                                             <Link
-                                                href="/login"
-                                                className="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold hover:bg-gray-50"
+                                                href="/"
+                                                onClick={handleLogout}
+                                                className="text-sm/6 font-semibold"
                                             >
-                                                Log in
+                                                Log out <span aria-hidden="true">&rarr;</span>
                                             </Link>
-                                        </>
+                                        </div>
+                                    ) : (
+                                        <Link
+                                            href="/login"
+                                            className="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold bg-ascent text-white text-center mt-4"
+                                        >
+                                            Get Started
+                                        </Link>
                                     )}
                                 </div>
                             </div>
