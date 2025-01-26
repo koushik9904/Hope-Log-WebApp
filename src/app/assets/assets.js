e3d9e2e9422c77c718bff4c5fb1c8af5c45c0f8d
LogicUI <johnwee35@gmail.com>
2025-01-26 22:01:51 +0800

    add new icons assets

--- src/app/assets/assets.js
+++ src/app/assets/assets.js
@@ -16,12 +16,45 @@ import CircleCheck from './icons/circle-check-solid.svg'
 import Exclamation from './icons/exclamation-solid.svg'
 import MicrophoneIcon from './icons/microphone-solid.svg'
 import StopIcon from './icons/stop-solid.svg'
+import BrainSolid from './icons/brainSolid.svg'
+import ChartLineSolid from './icons/chartLineSolid.svg'
+import LightBulbSolid from './icons/lightBulbSolid.svg'
+import LockSolid from './icons/lockSolid.svg'
+import MobileSolid from './icons/mobileSolid.svg'
+import WandMagicSolid from './icons/wandMagicSolid.svg'
+import ChatHero from './images/chatExample.png'
+import EntriesHero from './images/entries.png'
+import WritingSold from './icons/pen-to-square-regular.svg'
+import RobotSolid from './icons/robot-solid.svg'
+import BullsEyeSolid from './icons/bullseye-solid.svg'
+import ChartPieSolid from './icons/chart-pie-solid.svg'
+import MoodTrendsChart from './icons/mood-trends-chart.svg'
+import HalfShield from './icons/halfShield.svg'
+import Certificate from './icons/certificateSolid.svg'
+import KeySolid from './icons/keySolid.svg'
+import UserShield from './icons/userShield.svg'
 
+export const HalfShieldSVG = HalfShield
+export const CertificateSVG = Certificate
+export const KeySolidSVG = KeySolid
+export const UserShieldSVG = UserShield
 
+export const BullsEyeSolidSVG = BullsEyeSolid
+export const ChartPieSolidSVG = ChartPieSolid
+export const MoodTrendsChartSVG = MoodTrendsChart
+export const LightBulbSolidSVG = LightBulbSolid
+
+export const WritingSoldSVG = WritingSold
+export const RobotSolidSVG = RobotSolid
+export const BrainSolidSVG = BrainSolid
+export const ChartLineSolidSVG = ChartLineSolid
+export const LockSolidSVG = LockSolid
+export const MobileSolidSVG = MobileSolid
+export const WandMagicSolidSVG = WandMagicSolid
 export const BookIconSVG = BookIcon
 export const FaceIconSVG = FaceIcon
 export const GlobeIconSVG = GlobeIcon
-export const HopeIconSVG = HopeIcon 
+export const HopeIconSVG = HopeIcon
 export const SeedlingIconSVG = Seedling
 export const FaceAngrySVG = faceAngry
 export const FaceSadSVG = faceSad
@@ -36,3 +69,5 @@ export const CircleCheckSVG = CircleCheck
 export const ExclamationSVG = Exclamation
 export const MicrophoneIconSVG = MicrophoneIcon
 export const StopIconSVG = StopIcon
+export const ChatHeroPNG = ChatHero
+export const EntriesHeroPNG = EntriesHero
