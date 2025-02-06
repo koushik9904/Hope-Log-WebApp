5cc99dfce863de72597732f2a6ebd9c8fe1fce35
LogicUI <johnwee35@gmail.com>
2025-02-07 02:31:48 +0800

    add contact me to page component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,10 +6,11 @@ import AnalysisLanding from './components/AnalysisLanding'
 import PricingLanding from './components/PricingLanding'
 import FrequentlyAskedQuestions from './components/FrequentlyAskedQuestions'
 import PrivacyAndSecurityLanding from './components/PrivacyAndSecurityLanding'
+import ContactMe from './components/ContactMe'
 
 export default function Home() {
   return (
-    <main className="bg-dark text-white">
+    <main className="bg-dark text-white ">
       <HeroLanding />
       <JournalingOverview />
       <HowJournalingWorks />
@@ -17,6 +18,7 @@ export default function Home() {
       <PricingLanding />
       <FrequentlyAskedQuestions />
       <PrivacyAndSecurityLanding />
+      <ContactMe />
     </main >
   );
 }
